2ff175c58f511f34a35ae70ae78da7ca0f51ee39
Tayyaba Nasir <tayyabanasir133@gmail.com>
2026-01-04 14:21:19 +0500

    chap14-16

---
+++ Chap14-16/index.js
@@ -0,0 +1,73 @@
+//1-
+const students =[];
+console.log(students);
+students.push("Tayyaba","fatima");
+console.log(students);
+//2-Declare an empty array using JS object notation to store student names in future.
+var myStudents = {"students": [{"firstName": "", "lastName": ""}, {"firstName": "", "lastName": ""}
+
+]};
+console.log(myStudents);
+firstName = "Tayyaba";
+lastName = "Nasir";
+console.log(firstName, lastName);
+
+//3-Declare and initialize a strings array.
+var Array = ["Tayyaba", "Fatima", "Ayesha", "Zainab"];
+console.log(Array);
+
+//4-Declare and initialize a numbers array.
+var num = [0,1, 2, 3, 4, 5];
+console.log(num);
+
+//5-Declare and initialize a boolean array.
+var boolean = [true, false]
+console.log(boolean);
+
+//6-Declare and initialize a mixed array.
+var mixedArray = ["Tayyaba", 1, true, "Fatima", 2, false];
+console.log(mixedArray);
+
+//7-Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser like:
+var qualifications = ["1)SSC", "2)HSC", "3)BCS", "4)BS", "5)BCOM", "6)MS", "7)M. Phil.", "8)PhD"];
+document.write("<h1>Qualifications:</h1>");
+//qualifications.unshift("");
+document.write("<br>" + qualifications[0],"<br>" + qualifications[1],"<br>" + qualifications[2],"<br>" + qualifications[3],"<br>" + qualifications[4],"<br>" + qualifications[5],"<br>" + qualifications[6],"<br>" + qualifications[7]);
+
+//8-Declare and initialize an array and store the names of students and their scores & show the scores & percentages of students like:
+var names = ["Tayyaba", "Fatima", "Ayesha"];
+var scores = [450, 300, 350];
+var totalMarks = 500;
+document.write("<br><br><h3>Score of " + names[0] + " is " + scores[0] + ". Percentage: " + (scores[0]/totalMarks)*100 + "%" +"<br><br>" + names[1] + " is " + scores[1] + ". Percentage: " + (scores[1]/totalMarks)*100 + "%" +"<br><br>" + names[2] + " is " + scores[2] + ". Percentage: " + (scores[2]/totalMarks)*100 + "%</h3>");
+
+//9-Initialize an array with color names. Display the array elements in your browser.
+var colors = ["Red", "Green", "Blue", "Yellow", "Purple"];
+document.write("<br><br><h3>Colors:</h3>" + colors);
+//a-Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array. Display the updated array in your browser.
+var ColorAtBeginning = prompt("Which color do you want to add to the beginning?");
+colors.unshift(ColorAtBeginning);
+document.write("<br><br><h3>he/she wants to add to the beginning:</h3>" + colors);
+//b-Ask the user what color he/she wants to add to the end & add that color to the end of the array. Display the updated array in your browser.
+var ColorAtEnd = prompt("Which color do you want to add to the end?");
+colors.push(ColorAtEnd);
+document.write("<br><br><h3>he/she wants to add at the end:</h3>" + colors);
+//c-Add two more colors to the beginning of the array. Display the updated array in your browser.
+colors.unshift("Orange", "Pink");
+document.write("<br><br><h3>Add two more colors to the beginning of the array:</h3>" + colors);
+//d-Delete the first color in the array. Display the updated array in your browser.
+colors.shift();
+document.write("<br><br><h3>Delete the first color in the array:</h3>" + colors);
+//e-Delete the last color in the array. Display the updated array in your browser.
+colors.pop();
+document.write("<br><br><h3>Delete the last color in the array:</h3>" + colors);
+//f-Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index. Display the updated array in your browser.
+var AddAtIndex = prompt("At which index do you want to add a color?");
+var ColorName = prompt("Which color do you want to add?");
+colors.splice(AddAtIndex, 0, ColorName);
+document.write("<br><br><h3>Add the color to desired position/index:</h3>" + colors);
+//g-Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then remove the same number of color(s) from user-defined position/index. Display the updated array in your browser.
+var DeleteAtIndex = prompt("At which index do you want to delete color(s)?");
+var HowManyColors = prompt("How many colors do you want to delete?");
+colors.splice(DeleteAtIndex, HowManyColors);
+document.write("<br><br><h3>Remove the same number of color(s) from user-defined position/index:</h3>" + colors);
+//10-Write a program to store student scores in an array & sort the array in ascending order using Array’s sort method.
