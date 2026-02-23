7d942bb2a8489282a5b900b24c6638945b60e2d5
Tayyaba Nasir <tayyabanasir133@gmail.com>
2026-02-23 08:16:32 +0500

    chap43-48

---
+++ chap 43-48/app.js
@@ -0,0 +1,22 @@
+function mylink(e) {
+    alert("It's working!")
+}
+
+function mybtn(e) {
+    alert("Thnaks for purchasing phone from us !")
+}
+
+function rowdlt(e) {
+   e.parentNode.removeChild(e)
+
+} 
+
+function firstimg(e) {
+    e.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTATsiOFKsGasx693WvXi6ruJE3G-2G2a0p_w&s"
+  
+}
+
+function secimg(e){
+    e.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQS_aMhEbPyZg5vZO0cxHNkLozmUAAub-2IQ&s"
+
+}
